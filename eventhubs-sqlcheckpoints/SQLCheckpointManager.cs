ce249f40f30ddd8af78df5a253bd5dca56478d08
Mikhail Shilkov <mshilkov@astrata.eu>
2017-05-29 16:06:02 +0200

    Reliable event hubs processing samples

---
+++ eventhubs-sqlcheckpoints/SQLCheckpointManager.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Azure.EventHubs.Processor;
+
+public class SQLCheckpointManager : ICheckpointManager
+{
+    private readonly string connectionString;
+    private readonly string topic;
+
+    public TimeSpan LeaseRenewInterval => TimeSpan.FromDays(1);
+
+    public TimeSpan LeaseDuration => TimeSpan.MaxValue;
+
+    public SQLCheckpointManager(string connectionString, string topic)
+    {
+        this.connectionString = connectionString;
+        this.topic = topic;
+    }
+
+    public async Task<bool> CheckpointStoreExistsAsync()
+    {
+        var connection = new SqlConnection(this.connectionString);
+        var table = await connection.QueryFirstOrDefaultAsync(
+            new CommandDefinition("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'EventHubCheckpoint'"));
+        return table != null;
+    }
+
+    public async Task<bool> CreateCheckpointStoreIfNotExistsAsync()
+    {
+        var connection = new SqlConnection(this.connectionString);
+        var result = await connection.ExecuteScalarAsync<int>(
+            @"IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'EventHubCheckpoint')
+                BEGIN
+                CREATE TABLE EventHubCheckpoint (
+                    Topic varchar(100) NOT NULL,
+                    PartitionID varchar(100) NOT NULL,
+                    SequenceNumber bigint NOT NULL,
+                    Offset varchar(20) NOT NULL,
+                    CONSTRAINT PK_EventHubCheckpoint PRIMARY KEY CLUSTERED (Topic, PartitionID)
+                )
+                SELECT 1
+                END
+                ELSE SELECT 0");
+        return result > 0;
+    }
+
+    public async Task<Checkpoint> CreateCheckpointIfNotExistsAsync(string partitionId)
+    {
+        var connection = new SqlConnection(this.connectionString);
+        var checkpoint = await connection.QueryFirstOrDefaultAsync(
+            new CommandDefinition(
+                "SELECT SequenceNumber, Offset FROM EventHubCheckpoint WHERE PartitionID = @PartitionID AND Topic = @Topic",
+                new { PartitionID = partitionId, Topic = this.topic }))
+            .ConfigureAwait(false);
+
+        if (checkpoint == null)
+        {
+            var result = new Checkpoint(partitionId);
+
+            await connection.ExecuteAsync(
+                "INSERT EventHubCheckpoint (Topic, PartitionID, SequenceNumber, Offset) VALUES (@Topic, @PartitionID, @SequenceNumber, @Offset)",
+                new { Topic = this.topic, PartitionID = partitionId, SequenceNumber = result.SequenceNumber, Offset = result.Offset });
+
+            return result;
+        }
+
+        return new Checkpoint(partitionId)
+        {
+            Offset = checkpoint.Offset,
+            SequenceNumber = checkpoint.SequenceNumber
+        };
+    }
+
+    public Task DeleteCheckpointAsync(string partitionId)
+    {
+        // Make this a no-op to avoid deleting by accident.
+        return Task.FromResult(0);
+    }
+
+    public async Task<Checkpoint> GetCheckpointAsync(string partitionId)
+    {
+        var connection = new SqlConnection(this.connectionString);
+        var checkpoint = await connection.QueryFirstOrDefaultAsync(
+            new CommandDefinition(
+                "SELECT SequenceNumber, Offset FROM EventHubCheckpoint WHERE PartitionID = @PartitionID AND Topic = @Topic",
+                new { PartitionID = partitionId, Topic = this.topic }))
+            .ConfigureAwait(false);
+        return checkpoint != null
+            ? new Checkpoint(partitionId)
+            {
+                Offset = checkpoint.Offset,
+                SequenceNumber = checkpoint.SequenceNumber
+            }
+            : null;
+    }
+
+    [Obsolete("Use UpdateCheckpointAsync(Lease lease, Checkpoint checkpoint) instead", true)]
+    public Task UpdateCheckpointAsync(Checkpoint checkpoint)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateCheckpointAsync(Lease lease, Checkpoint checkpoint)
+    {
+        var connection = new SqlConnection(this.connectionString);
+        await connection.ExecuteAsync(
+                "UPDATE EventHubCheckpoint SET SequenceNumber = @SequenceNumber, Offset = @Offset WHERE PartitionID = @PartitionID AND Topic = @Topic",
+                new { PartitionID = lease.PartitionId, SequenceNumber = checkpoint.SequenceNumber, Offset = checkpoint.Offset, Topic = this.topic });
+    }        
+}
---
+++ eventhubs-sqlcheckpoints/SQLDeadLetterManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Azure.EventHubs;
+
+public class DeadLetter<T>
+{
+    public DeadLetter(T data, DateTime failureTime, Exception exception)
+    {
+        this.Data = data;
+        this.FailureTime = failureTime;
+        this.Exception = exception;
+    }
+
+    public T Data { get; }
+
+    public DateTime FailureTime { get; }
+
+    public Exception Exception { get; }
+}
+
+public interface IDeadLetterManager
+{
+    Task AddFailedEvents(IEnumerable<DeadLetter<EventData>> deadLetters);
+
+    Task<bool> CreateDeadLetterStoreIfNotExistsAsync();
+}
+
+public class SQLDeadLetterManager : IDeadLetterManager
+{
+    private readonly string connectionString;
+    private readonly string topic;
+
+    public SQLDeadLetterManager(string connectionString, string topic)
+    {
+        this.connectionString = connectionString;
+        this.topic = topic;
+    }
+
+    public Task AddFailedEvents(IEnumerable<DeadLetter<EventData>> deadLetters)
+    {
+        var connection = new SqlConnection(this.connectionString);
+        return connection.ExecuteAsync(
+            @"insert EventHubDeadLetter (Topic,PartitionID,SequenceNumber,Offset) 
+            values(@Topic,@PartitionID,@SequenceNumber,@Offset)",
+            deadLetters
+                .Select(dl => new
+                {
+                    Topic = this.topic,
+                    PartitionID = dl.Data.SystemProperties.PartitionKey,
+                    dl.Data.SystemProperties.SequenceNumber,
+                    dl.Data.SystemProperties.Offset,
+                    dl.FailureTime,
+                    Error = dl.Exception.ToString()
+                }));
+    }
+
+    public async Task<bool> CreateDeadLetterStoreIfNotExistsAsync()
+    {
+        var connection = new SqlConnection(this.connectionString);
+        var result = await connection.ExecuteScalarAsync<int>(
+            @"IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'EventHubDeadLetter')
+                BEGIN
+                CREATE TABLE EventHubDeadLetter (
+                    Topic varchar(100) NOT NULL,
+                    PartitionID varchar(100) NOT NULL,
+                    SequenceNumber bigint NOT NULL,
+                    Offset varchar(20) NOT NULL,
+                    FailedAt datetime NOT NULL,
+                    Error nvarchar(max) NOT NULL,
+                    CONSTRAINT PK_EventHubDeadLetter PRIMARY KEY CLUSTERED (Topic, PartitionID)
+                )
+                SELECT 1
+                END
+                ELSE SELECT 0");
+        return result > 0;
+    }
+}
